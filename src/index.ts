489bebbf4f2b35d81ff14fef1bc09680938b6107
Om Jee Mishra <omjeem558@gmail.com>
2025-05-15 04:00:27 +0530

    update cors origin

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://shiksha-tech-web.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   // allowedHeaders: ["Content-Type", "Authorization"],
